29f9cf55d06acdf687821dd528813701b4aaf4b1
MAX <maksimdemidovru@mail.ru>
2022-08-19 09:44:30 +0300

    add min

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,13 +3,22 @@
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int max = a;
+int min = 0;
 
 if (max < b)
 {
     max = b;
+    min = a;
+    Console.Write("max = ");
     Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.Write(a);
 }
 if (max > b)
 {
+    min = b;
+    Console.Write("max = ");
     Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.Write(b);
 }
