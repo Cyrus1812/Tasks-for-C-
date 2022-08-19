3fda3030a8c35330954946166f8f96298de5f44d
MAX <maksimdemidovru@mail.ru>
2022-08-19 14:11:07 +0300

    add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+int a = x % 2;
+
+if (a == 0)
+{
+    Console.WriteLine("число чётное");
+}
+else
+{
+    Console.WriteLine("число нечётное");
+}
+
