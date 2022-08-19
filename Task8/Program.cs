86af6cd48073a7e62f7e0bd2a6000decd6fe3573
MAX <maksimdemidovru@mail.ru>
2022-08-19 16:27:35 +0300

    add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое положительное число ");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int a = 1; a <= x; a++)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+}
