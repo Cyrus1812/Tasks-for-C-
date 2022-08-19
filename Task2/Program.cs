ce7cbb5f9f64438ff0d7bdb1575312796eef83d7
MAX <maksimdemidovru@mail.ru>
2022-08-19 09:31:06 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Впишите 2 числа ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (max < b)
+{
+    max = b;
+    Console.WriteLine(b);
+}
+if (max > b)
+{
+    Console.WriteLine(a);
+}
